9a1d58030ac38d7af3f0a32222210855badc4414
ANMOL SALARIA <anmolsingh@ANMOLs-MacBook-Pro.local>
2025-03-29 04:45:02 +0530

    Changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCc82pQeABA942Gb9DeAqI1263kJexjN5w",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
